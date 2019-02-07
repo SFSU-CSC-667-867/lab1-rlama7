97f63d2e760dadfd3d158b113b5be6f1281dfd22
rlama7 <royalray7@gmail.com>
2019-02-06 20:57:58 -0800

    modules tutorial in javascript.

--- node/files.js
+++ node/files.js
@@ -1,10 +1,15 @@
+// jshint esversion:6
+
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
 
 fs.readFile(path.join(__dirname, 'hello.txt'), { encoding: 'utf8' }, (err, txt) => {
   // this triggers when the read is complete
   if (err) return console.log(err);
   console.log(txt);
 });
+
 // This triggers directly after asking to read the file
 console.log('Just asked for file');
+console.log(os.cpus());
