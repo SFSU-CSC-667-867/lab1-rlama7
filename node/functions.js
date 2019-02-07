4f9972109f79be455c1d169de1cc4577b5768962
rlama7 <royalray7@gmail.com>
2019-02-06 20:28:13 -0800

    Intro to JavaScript Functions.

--- node/functions.js
+++ node/functions.js
@@ -1,3 +1,4 @@
+// jshint esversion: 6
 // Declaring with the function keyword
 function hi() {
   console.log('Say Hello');
@@ -8,12 +9,25 @@ hi();
 // arrow functions
 const arrow = (test) => {
   console.log(test);
-}
+};
 
-arrow('hi')
+arrow('hi');
 
 // for practice
 
 // make an arrow function with no block body
+const myArrow = (y) => y + 1;
+
+// {
+//   return y + 1;
+// }
+console.log(myArrow(8));
 
 // make a function that returns a function
+const functionFatory = () => () => console.log('This is weird');
+
+const a = functionFatory(); // or use ()() to avoid line 32
+a();
+
+const anotherFunction = functionFatory;
+anotherFunction()();
